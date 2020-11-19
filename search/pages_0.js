a87c7713f145a08a3d457a16f4f1278f99331c0d
Jakub Kalinowski <kalina559@gmail.com>
2020-11-19 18:34:34 +0100

    updated documentation

--- search/pages_0.js
+++ search/pages_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['readme_88',['README',['../md__c___users__kalin__desktop_zaba__frog-vs_8-the-_street__r_e_a_d_m_e.html',1,'']]]
+  ['readme_88',['README',['../md__c__dev__frog-vs_8-the-_street__r_e_a_d_m_e.html',1,'']]]
 ];
